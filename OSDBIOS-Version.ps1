c4f0352b5a24b4930d0b3a160c653ea88038b3ac
Biggoan1 <17280612+Biggoan1@users.noreply.github.com>
2018-09-18 14:37:52 -0400

    Create OSDBIOS-Version.ps1
    SMBIOSBIOSVersion is stored as a string, not an integer. This script will create a BIOS version number TS Variable in the 000.000.000 format making it easier to upgrade Dell BIOS during a task sequence.

---
+++ OSDBIOS-Version.ps1
@@ -0,0 +1,51 @@
+<#
+.SYNOPSIS
+Get Dell BIOS Version During OSD
+
+.DESCRIPTION
+SMBIOSBIOSVersion is stored as a string, not an integer. This script will create a BIOS version number TS Variable. 
+
+Content source structure:
+OSDBIOS-Version
+
+.EXAMPLE
+
+.PARAMETER 
+
+.NOTES
+1.0 Initial Release 
+
+Currently only verified with Dell Machines. 
+
+#>
+
+# Setup the TSEnvironment for OSD
+$TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Stop
+
+# Get the BIOS Version from WMI 
+$OSDBIOSVersion = Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SMBIOSBIOSVersion 
+
+    if ($OSDBIOSVersion -like "A*")
+        { 
+            # If this is a legacy Dell BIOS basically do nothing. 
+            $TSEnvironment.Value("SMSTSBiosVersion") = $OSDBIOSVersion 
+        }
+    else
+        {
+            # Convert the BIOS version to int and split it. 
+            [int]$Major,[int]$Minor,[int]$Build = $OSDBIOSVersion.Split('{.}')
+
+            # If BIOS Version is less than 100 then this will add the leading zeros. 
+            if ($Major -lt 100) { $Major1 = $Major.ToString("000") } else { $Major1 = $Major }
+            if ($Minor -lt 100) { $Minor1 = $Minor.ToString("000") } else { $Minor1 = $Minor }
+            if ($MBuild -lt 100) { $Build1 = $Build.ToString("000") } else { $Build1 = $Build } 
+
+            # Set the version with leading zeros. 
+            $OSDBIOSVersion = "$Major1.$Minor1.$Build1"
+            #$OSDBIOSVersion #Uncomment for testing
+        }
+# Sets SMSTSBiosVersion as a TS Variable. 
+$TSEnvironment.Value("SMSTSBiosVersion") = $OSDBIOSVersion 
+
+# Exit script. 
+exit 1
