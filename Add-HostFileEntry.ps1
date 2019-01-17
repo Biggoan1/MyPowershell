15882b32d03cd3a0f16b2a2ab7a1c18d342aec61
Biggoan1 <17280612+Biggoan1@users.noreply.github.com>
2019-01-17 10:48:38 -0500

    Add files via upload

---
+++ Add-HostFileEntry.ps1
@@ -0,0 +1,9 @@
+﻿$IPAddress = ""
+$ServerName = ""
+$IPAddress1 = ""
+$ServerName1 = ""
+
+Start-Process powershell -Verb runAs -ArgumentList '-NoProfile -command "& { ` 
+    Add-content -path "C:\Windows\System32\drivers\etc\hosts" -value """$IPAddress `t $Servername""" ; ` 
+    Add-content -path "C:\Windows\System32\drivers\etc\hosts" -value """$IPAddress1 `t $ServerName1"""}"'
+
