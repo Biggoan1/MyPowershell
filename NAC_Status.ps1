44eeec834ab35ddf9826c754720983c3e85b623f
Biggoan1 <17280612+Biggoan1@users.noreply.github.com>
2019-08-21 10:47:55 -0400

    Add files via upload

---
+++ NAC_Status.ps1
@@ -0,0 +1,14 @@
+﻿# Setup the TSEnvironment for OSD
+$TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Stop
+
+$Auth = netsh lan show interface | sls 'State' | ? { $_ -match 'Connected. Authentication succeeded.'}
+
+if ($Auth -like "*Connected. Authentication succeeded.") 
+    { 
+        $TSEnvironment.Value("NACStatus") = $true 
+    } 
+else 
+    { 
+        $TSEnvironment.Value("NACStatus") = $false
+    } 
+
